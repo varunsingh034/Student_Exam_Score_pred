bcfbeaedff37ceae1052c9e61e2a74d01605c9a8
Varun Singh <varunbaroliya034@gmail.com>
2025-02-19 21:44:22 +0530

    Project origin

---
+++ spf.py
@@ -0,0 +1,41 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score
+from sklearn.preprocessing import OrdinalEncoder
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+spf_df = pd.read_csv('D:\Programs\PYTHON\Project\SPF\spf.csv')
+
+spf_df.drop(columns=['School_Type','Parental_Education_Level','Gender','Internet_Access',
+                     'Learning_Disabilities','Distance_from_Home','Extracurricular_Activities',
+                     'Parental_Involvement','Family_Income','Teacher_Quality'], inplace=True)
+
+spf_df['Exam_Score'].replace(101,100,inplace=True)
+
+x_train, x_test, y_train, y_test = train_test_split(spf_df.iloc[:,:9], spf_df.iloc[:,9], test_size=0.2,random_state=42)
+
+oe_atr = OrdinalEncoder(categories=[['Low','Medium','High']])
+x_train_atr = oe_atr.fit_transform(x_train[['Access_to_Resources']])
+x_test_atr = oe_atr.transform(x_test[['Access_to_Resources']])
+
+oe_ml = OrdinalEncoder(categories=[['Low','Medium','High']])
+x_train_ml = oe_ml.fit_transform(x_train[['Motivation_Level']])
+x_test_ml = oe_ml.transform(x_test[['Motivation_Level']])
+
+oe_pi = OrdinalEncoder(categories=[['Negative','Neutral','Positive']])
+x_train_pi = oe_pi.fit_transform(x_train[['Peer_Influence']])
+x_test_pi = oe_pi.transform(x_test[['Peer_Influence']])
+
+x_train_rem = x_train.drop(columns=['Access_to_Resources','Motivation_Level','Peer_Influence'])
+x_test_rem = x_test.drop(columns=['Access_to_Resources','Motivation_Level','Peer_Influence'])
+
+x_train_main = np.concatenate((x_train_rem,x_train_atr,x_train_ml,x_train_pi),axis=1)
+x_test_main = np.concatenate((x_test_rem,x_test_atr,x_test_ml,x_test_pi),axis=1)
+
+lr = LinearRegression()
+lr.fit(x_train_main,y_train)
+
+y_pred_lr = lr.predict(x_test_main)
+
+print(r2_score(y_test, y_pred_lr))
---
+++ spf_load.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pickle
+
+oe_atr = pickle.load(open("D:\Programs\PYTHON\Project\SPF\oe_atr.pkl", "rb"))
+oe_ml = pickle.load(open("D:\Programs\PYTHON\Project\SPF\oe_ml.pkl", "rb"))
+oe_pi = pickle.load(open("D:\Programs\PYTHON\Project\SPF\oe_pi.pkl", "rb"))
+lr = pickle.load(open("D:\Programs\PYTHON\Project\SPF\lr_model.pkl", "rb"))
+
+test_input = np.array([10,70,7,80,4,4,'High','Medium','Neutral'],dtype=object).reshape(1, -1)
+
+test_input_atr = oe_atr.transform(test_input[:,6].reshape(1, 1))
+test_input_ml = oe_ml.transform(test_input[:,7].reshape(1, 1)) 
+test_input_pi = oe_pi.transform(test_input[:,8].reshape(1, 1))
+
+test_input_transformed = np.concatenate((test_input[:,0:6], test_input_atr, test_input_ml, test_input_pi), axis=1)
+
+test_pred = lr.predict(test_input_transformed)
+
+print(test_pred)
+
